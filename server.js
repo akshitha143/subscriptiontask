fa247c54fbb130219b3cdfa98489ee2f4833c129
akshitha143 <akshithaoleti@gmail.com>
2025-05-29 17:37:39 +0530

    server

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 
 const subscriptionRoutes = require('./routes/subscriptionRoutes');
 
-app.use('/subscriptions', subscriptionRoutes);
+app.use('/api/subscriptions', subscriptionRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
